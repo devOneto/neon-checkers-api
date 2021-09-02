74c53bcde1a1fea1caa96e4cfe768356cdb3a88b
NetoAraujo <bipnerds@gmail.com>
2021-09-02 01:10:41 -0300

    tabuleiro reagindo à música

--- src/index.js
+++ src/index.js
@@ -30,53 +30,55 @@ function setup() {
 function draw() {
   background(30);
 
+  media = fft.analyze().reduce(function (a,b) {return a+b}) / 96;
+  if(media>255){media=media/2}
   // eq.bands[5].gain(40);
   // eq.bands[5].gain(-30);
 
   let rms = analyzer.getLevel()*5;
-  let r = random(100,200) + rms * 40
-  let g = 80
-  let b = 20
+  let r = media
+  let g = 0
+  let b = 0
 
   //desenhar casas pares
-  // fill(r,g,b)
-  // for (let i = 0; i < 4; i++) {
-  //   for (let j = 0; j < 8; j++) {
-  //     if(j%2!=0){
-  //       rect(70+i*2*lc,10+j*lc,lc,lc)
-  //     }else{
-  //       rect(10+2*i*lc,10+j*lc,lc,lc)
-  //     }
-  //   }
-  // }
-  // fill(255)
+  fill(r,g,b)
+  for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < 8; j++) {
+      
+      if(j%2!=0){
+        rect(70+i*2*lc,10+j*lc,lc,lc)
+      }else{
+        rect(10+2*i*lc,10+j*lc,lc,lc)
+      }
+    }
+  }
+  fill(255)
 
-  // //desenhar casas ímpares
-  // fill(30,30,30)
-  // for (let i = 0; i < 4; i++) {
-  //   for (let j = 0; j < 8; j++) {
-  //     if (j % 2 == 0) {
-  //       rect(70+i*2*lc,10+lc*j,lc,lc)
-  //     }else{
-  //       rect(10+i*2*lc,10+lc*j,lc,lc)
-  //     }
-  //   }
-  // }
+  //desenhar casas ímpares
+  fill(30,30,30)
+  for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < 8; j++) {
+      if (j % 2 == 0) {
+        rect(70+i*2*lc,10+lc*j,lc,lc)
+      }else{
+        rect(10+i*2*lc,10+lc*j,lc,lc)
+      }
+    }
+  }
 
   fill(255)
 
-  // text(eq.bands,10,20);
+//Equalizer
+// for(i = 0; i < 99; i++){
+//   y = 400-fft.analyze()[i];
+//   rect(5*i+5,y,2.5,2.5)
+// }
 
-for(i = 0; i < 99; i++){
-  y = 400-fft.analyze()[i];
-  rect(5*i+5,y,2.5,2.5)
-}
+// media = fft.analyze().reduce(function (a,b) {return a+b}) / 96
+// line(0,400-media,800,400-media)
 
-media = fft.analyze().reduce(function (a,b) {return a+b}) / 96
-line(0,400-media,800,400-media)
-
-fill(media,100 ,100)
-rect(10,10,20,20)
+// fill(media,100 ,100)
+// rect(10,10,20,20)
 
 }
 
